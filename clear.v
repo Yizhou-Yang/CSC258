c4401bedd3d7b35fc49dc6b27130736392b44a40
Yizhou-Yang <32808678+Yizhou-Yang@users.noreply.github.com>
2019-11-28 11:56:50 -0500

    clear fixed

--- clear.v
+++ clear.v
@@ -6,49 +6,51 @@ module clearCards(reset_n,clk,in,x0, y0, x, y, colour, next);
 	output [6:0] y;
 	output[2:0] colour;
 	output reg next;
-	
+	//
+	wire [7:0] count;
+	//
 	assign colour = 3'b000;
-	addtoxy xy(.x(x0), .y(y0),.in(in), .reset_n(reset_n) ,.clk(clk), .x_out(x), .y_out(y));
+	
+
+	add a1(.x(x0), .y(y0),.in(in), .reset_n(reset_n) ,.clk(clk), .x_out(x), .y_out(y),.count(count));
 
 	always @(*)
 	begin
-			if(x == (x0 + 15) && y == (y0 + 15)) 
-				next <= 1;
-			if(x == (x0 + 3) && y == (y0 + 3)) 
-				next <= 0;
-	end
-	
-	always @(*)
-	begin
-		if (!reset_n)
-			next = 0;
+			if(!reset_n)
+					next <= 0;
+			else if(x == (x0 + 15) && y == (y0 + 15)) 
+				if(count == 0)
+					next <= 1;
+			else if(x == (x0 + 1) && y == (y0 + 1)) 
+					next <= 0;
 	end
 endmodule
 
-module addtoxy(x, y, in, reset_n, clk, x_out, y_out);
+module add(x, y, in, reset_n, clk, x_out, y_out,count);
 	input [7:0] x;
 	input [6:0] y;
 	input reset_n, in;
 	input clk;
 	output reg [7:0] x_out;
 	output reg [6:0] y_out;
+	//
+	output reg[7:0] count;
+	//
 	
-	reg[7:0] count;
-	
-	always @(posedge clk)
-	begin
-		count <= count + 1;
-	end
-	
-	always @(posedge in or negedge reset_n)begin
-			x_out <= 0;
-			y_out <= 0;
+	always @(posedge clk)begin
+		if(!reset_n)
 			count <= 0;
+		else if(in)
+			count <= count + 1;
 	end
 	
 	always @(*)
 	begin
-		if(in) begin
+		if (!reset_n)begin
+			x_out = 0;
+			y_out = 0;
+		end
+		else if(in) begin
 			x_out = x + count[3:0];
 			y_out = y + count[7:4];
 		end
